f6b198fa74a4cb6c845c7ff8a697a30a0021bb04
axioTake <fun467x@yahoo.de>
2020-04-18 19:08:38 +0200

    Add Laravel versions 6.*, 7.*

--- src/Models/Permission.php
+++ src/Models/Permission.php
@@ -12,11 +12,37 @@ class Permission extends Model implements PermissionHasRelationsContract
     use Slugable, PermissionHasRelations;
 
     /**
+     * The attributes that are not mass assignable.
+     *
+     * @var array
+     */
+    protected $guarded = [ 'id' ];
+
+    /**
      * The attributes that are mass assignable.
      *
      * @var array
      */
     protected $fillable = ['name', 'description', 'model'];
+
+    /**
+     * Typecast for protection.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'id'            => 'integer',
+        'name'          => 'string',
+        'description'   => 'string',
+        'model'         => 'string',
+    ];
+
+    /**
+     * Indicates if the model should be timestamped.
+     *
+     * @var bool
+     */
+    public $timestamps = true;
 
     /**
      * Create a new model instance.
--- src/Models/Role.php
+++ src/Models/Role.php
@@ -12,11 +12,37 @@ class Role extends Model implements RoleHasRelationsContract
     use Slugable, RoleHasRelations;
 
     /**
+     * The attributes that are not mass assignable.
+     *
+     * @var array
+     */
+    protected $guarded = [ 'id' ];
+
+    /**
      * The attributes that are mass assignable.
      *
      * @var array
      */
     protected $fillable = ['name', 'description', 'level'];
+
+    /**
+     * Typecast for protection.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'id'            => 'integer',
+        'name'          => 'string',
+        'description'   => 'string',
+        'level'         => 'integer',
+    ];
+
+    /**
+     * Indicates if the model should be timestamped.
+     *
+     * @var bool
+     */
+    public $timestamps = true;
 
     /**
      * Create a new model instance.
